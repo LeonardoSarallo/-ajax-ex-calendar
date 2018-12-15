0308e51bd2ce9fa50c01d8111f321e481b0e85ff
LeonardoSarallo <leo.sarallo@gmail.com>
2018-12-15 16:32:13 +0100

    hambmenu

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 $(document).ready(function() {
   var date = moment('2017-01-01');
   print(date);
+  addHolidays(date);
 
   $.ajax({
     url:'https://holidayapi.com/v1/holidays',
@@ -13,7 +14,22 @@ $(document).ready(function() {
     },
     success: function(data)
     {
-      console.log(data);
+      var holidays = data.holidays;
+      $('ul li').each(function() {
+        var thisDate = $(this).attr('data-original_date');
+        thisDate = moment(thisDate, 'YYYY-MM-D');
+        console.log(thisDate);
+        for (var i = 0; i < holidays.length; i++) {
+          var holiday = holidays[i];
+          var holidayDate = moment(holiday.date);
+
+          if (holidayDate.isSame(thisDate, 'day'))
+          {
+            $(this).addClass('active');
+            $(this).append(' - ' + holiday.name);
+          }
+        }
+      });
     },
     error: function()
     {
@@ -24,13 +40,28 @@ $(document).ready(function() {
   $('#next').click(function() {
     date = date.add(1, 'months');
     print(date);
+    addHolidays(date);
   });
 
   $('#prev').click(function() {
     date = date.subtract(1, 'months');
     print(date);
+    addHolidays(date);
   });
 
+  $(document).on('click', '#hamb', function() {
+    var menuHamb = $('.month');
+    
+    if (menuHamb.hasClass('open')) {
+      menuHamb.removeClass('open');
+    }
+    else {
+      $('.dropdownmenu').removeClass('open');
+      menuHamb.addClass('open');
+
+    }
+
+  });
 
 
   function print(date)
@@ -43,6 +74,9 @@ $(document).ready(function() {
 
     for (var i = 1; i <= days; i++) {
       var template = $('.template li').clone();
+      var liData = date.format('YYYY-MM-') + i;
+
+      template.attr('data-original_date', liData)
       template.text(i + ' ' + date.format('MMM'));
 
       $('.monthdisplay ul').append(template);
@@ -52,29 +86,39 @@ $(document).ready(function() {
 
 });
 
+function addHolidays(date)
+{
+  $.ajax({
+    url:'https://holidayapi.com/v1/holidays',
+    method:'GET',
+    data: {
+      key: '6d29431d-847d-477b-b426-99c0f38d7a43',
+      country: 'IT',
+      month: date.format('MM'),
+      year: date.format('YYYY')
+    },
+    success: function(data)
+    {
+      var holidays = data.holidays;
+      $('ul li').each(function() {
+        var thisDate = $(this).attr('data-original_date');
+        thisDate = moment(thisDate, 'YYYY-MM-D');
+        console.log(thisDate);
+        for (var i = 0; i < holidays.length; i++) {
+          var holiday = holidays[i];
+          var holidayDate = moment(holiday.date);
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// var year = moment('2017-01');
-// var dayMonth = moment('2017-01').daysInMonth();
-// var days = moment(year).format('dddd')
-// console.log(dayMonth);
-//
-//
-// for (var i = 0; i < dayMonth; i++) {
-//
-// }
+          if (holidayDate.isSame(thisDate, 'day'))
+          {
+            $(this).addClass('active');
+            $(this).append(' - ' + holiday.name);
+          }
+        }
+      });
+    },
+    error: function()
+    {
+      alert('errore');
+    }
+  });
+}
